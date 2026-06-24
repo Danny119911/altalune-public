70044ff643dd869bab1b2f1ed28b26aa50762f63
Danny119911 <altalune.dev@gmail.com>
2026-06-23 21:08:38 -0400

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,5 +1,1 @@
-// Trigger gate fade‑in as soon as the user scrolls even 1 pixel
-window.addEventListener('scroll', () => {
-    document.body.classList.add('scrolled');
-}, { once: true });
-
+document.body.classList.add('scrolled');
